e5b9fddc5316c18c14e8a2a41f62cae50a5785da
Andrew Eymard <andrew@entrusttitle.com>
2020-03-06 16:18:18 +0530

    Developing

--- DictGen.py
+++ DictGen.py
@@ -1,2 +1,34 @@
 #!/usr/bin/env python3
 
+import string as st
+
+characters = ""
+
+number_of_words = input("Input the number of words required: ")
+print("{} words coming right up.".format(number_of_words))
+
+def required_characters():
+    global characters
+    print("Include lowercase letters? ")
+    ans = input("Y or N? ")
+    if ans == "Y":
+        characters += st.ascii_lowercase
+    if ans == "N":
+        exit
+    print("Include uppercase letters? ")
+    ans = input("Y or N? ")
+    if ans == "Y":
+        characters += st.ascii_uppercase
+    if  ans == "N":
+        exit
+    
+
+required_characters()
+print(characters)
+
+#i = 0
+#while i < number_of_words:
+
+#def word_gen(number_of_words):
+
+print("\nExit\n")
